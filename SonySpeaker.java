c5f1b0de48510e818214b5e793a5c2aa3681db42
limsemin <81283516+limsemin@users.noreply.github.com>
2023-01-04 14:43:25 +0900

    Add files via upload

---
+++ SonySpeaker.java
@@ -0,0 +1,14 @@
+package com.lec.basic.poly1;
+
+public class SonySpeaker {
+	
+	public SonySpeaker() {
+		System.out.println("===> sony Speaker 객체 생성");
+	}
+	public void volumeUp() {
+		System.out.println("===> Sony Speaker 불륨업");
+	}
+	public void volumeDown() {
+		System.out.println("===> Sony Speaker 불륨다운");
+	}
+}
